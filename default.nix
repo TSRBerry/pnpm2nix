c0d196d0352c7d5d629f1e4a02ba9e0348d4ba24
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-06-19 16:04:19 +0200

    Update for pnpm lockfile version 6.1

--- default.nix
+++ default.nix
@@ -198,7 +198,7 @@ in {
       };
 
   in
-    assert pnpmlock.lockfileVersion == "6.0";
+    assert pnpmlock.lockfileVersion == "6.1";
   (mkPnpmDerivation {
     deps = (builtins.map
       (attrName: packages."${attrName}")
